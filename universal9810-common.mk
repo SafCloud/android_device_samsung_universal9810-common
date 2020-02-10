023cfa6034d4ee34ba2df8ed1962a96774047824
synt4x93 <synt4x.93@gmail.com>
2020-02-10 02:29:58 +0000

    universal9810: build fingerprint@2.1-service.samsung

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -89,7 +89,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    fingerprint.exynos5
+    android.hardware.biometrics.fingerprint@2.1-service.samsung
 
 # Vibrator
 PRODUCT_PACKAGES += \
