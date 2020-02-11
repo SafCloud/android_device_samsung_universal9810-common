c3d3fd45bf0e19b48f6ce6fa89182b786a1a1572
synt4x93 <synt4x.93@gmail.com>
2020-02-11 03:55:49 +0000

    universal9810: graphics: build

--- universal9810-common.mk
+++ universal9810-common.mk
@@ -47,9 +47,24 @@ PRODUCT_PACKAGES += \
 # Treble
 PRODUCT_FULL_TREBLE_OVERRIDE := true
 
-# SP-NDK
+# Graphics
 PRODUCT_PACKAGES += \
-    libvulkan
+    libhwc2on1adapter \
+    android.hardware.graphics.allocator@2.0-impl \
+    android.hardware.graphics.allocator@2.0-service \
+    android.hardware.graphics.composer@2.2-impl \
+    android.hardware.graphics.composer@2.2-service \
+    android.hardware.graphics.common@2.1-impl \
+    android.hardware.graphics.mapper@2.0-impl \
+    android.hardware.memtrack@1.0-service \
+    android.hardware.memtrack@1.0-impl \
+    android.hardware.renderscript@1.0-impl \
+    libhwc2on1fbadapter \
+    libvulkan \
+    libtinyxml \
+    libion \
+    libfimg \
+    libc++
 
 # HIDL
 PRODUCT_COPY_FILES += \
